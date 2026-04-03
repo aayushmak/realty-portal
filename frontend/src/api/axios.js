e387c67435c3fe9e62098c89fdce8c72ac11a371
aayushmak <aayushmakafle@gmail.com>
2026-04-04 01:06:24 +0545

    failed to render

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://realty-portal.onrender.com/api',
+  baseURL: 'http://localhost:5001/api', // backend URL
   headers: { 'Content-Type': 'application/json' },
 });
 
