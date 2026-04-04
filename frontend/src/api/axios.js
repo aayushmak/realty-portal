cc906b9c5d8dd34c09a741d08080223f4f37da9e
aayushmak <aayushmakafle@gmail.com>
2026-04-04 12:27:41 +0545

    added render api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api', // backend URL
+  baseURL: 'http://localhost:5001/api'|| 'https://realty-portal.onrender.com/api', // backend URL
   headers: { 'Content-Type': 'application/json' },
 });
 
